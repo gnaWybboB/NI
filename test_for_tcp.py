ea581bb76f14f9788d0f8c20bd5c19be2f2719ad
gnaWybboB <wb1996@sjtu.edu.cn>
2017-02-17 14:41:35 +0800

    Revert "sync"
    This reverts commit 846000c8b3e39bfe0162cffb2e5bcc0f527b8bca.

--- test_for_tcp.py
+++ test_for_tcp.py
@@ -1,28 +1,9 @@
-#-*- coding: utf-8 -*-
-from socket import *
-
-class TcpClient:
-    HOST='192.168.1.110'
-    PORT=6340
-    BUFSIZ=1024
-    ADDR=(HOST, PORT)
-
-    def __init__(self):
-        self.client=socket(AF_INET, SOCK_STREAM)
-        self.client.connect(self.ADDR)
-        print ("has conneted")
-
-        while True:
-            data=input('>')
-            if not data:
-                break
-            #self.client.send(data.encode('utf8'))
-            data=self.client.recv(self.BUFSIZ)
-            if not data:
-                break
-            print(data.decode('utf8'))
-            
-if __name__ == '__main__':
-    client=TcpClient()
-    #add by 9
-	#add by b
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 28 22:28:29 2016
+@author: wb
+"""
+#引入模块
+import socket
+import threading
+import time
